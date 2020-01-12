69572843a000378aa3c8c805842cbdecbf11cf63
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-01-12 14:56:20 +0200

    Containerize backend

--- backend/src/utils/config.js
+++ backend/src/utils/config.js
@@ -1,3 +1,9 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+
+if (process.env.VINCIT_SUMMER_2020_DEV) {
+  dotenv.config({ path: '.env.development' });
+} else {
+  dotenv.config();
+}
 
 module.exports = { ...process.env };
