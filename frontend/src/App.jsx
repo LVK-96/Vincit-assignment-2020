c871e9afc29a9cf224c82357e7233cd92029464f
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-01-13 14:15:17 +0200

    Add loading screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import NewGameView from './Components/NewGameView';
 import OnGoingGameView from './Components/OnGoingGameView';
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [id, setId] = useState(null);
   const [points, setPoints] = useState(null);
   const [message, setMessage] = useState(null);
@@ -12,43 +13,49 @@ function App() {
     const idFromStorage = localStorage.getItem('id');
     if (idFromStorage) {
       setId(idFromStorage.toString());
-    }
-  }, []);
-
-  useEffect(() => {
-    if (id && points === null) {
       const fetchPoints = async () => {
         try {
-          const newPoints = await pointsService.getPoints(id);
+          const newPoints = await pointsService.getPoints(idFromStorage);
           setPoints(newPoints.amount);
+          setLoading(false);
         } catch (e) {
           window.alert('Failed to fetch points!');
         }
       };
 
       fetchPoints();
+    } else {
+      setLoading(false);
     }
-  }, [id, points]);
+  }, []);
 
-  const hasGame = !!id && (points !== null);
-  if (hasGame) {
+  if (!loading) {
+    const hasGame = !!id && (points !== null);
+    if (hasGame) {
+      return (
+        <div className="App">
+          <OnGoingGameView
+            setMessage={setMessage}
+            setPoints={setPoints}
+            setId={setId}
+            message={message}
+            points={points}
+            id={id}
+          />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-        <OnGoingGameView
-          setMessage={setMessage}
-          setPoints={setPoints}
-          setId={setId}
-          message={message}
-          points={points}
-          id={id}
-        />
+        <NewGameView setPoints={setPoints} setId={setId} />
       </div>
     );
   }
 
   return (
     <div className="App">
-      <NewGameView setPoints={setPoints} setId={setId} />
+      Loading...
     </div>
   );
 }
